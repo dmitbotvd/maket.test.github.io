0cbe97f9398ac0a5fc1c3ad311813b15b6d9a3f2
bot <30392154+dmitbotvd@users.noreply.github.com>
2019-03-26 22:37:27 +0200

    test

---
+++ assets/js/app.js
@@ -0,0 +1,33 @@
+$(function () {
+
+    var header = $("#header"),
+        introH = $("#intro").innerHeight(),
+        scrollOffset = $(window).scrollTop();
+    
+    /*Fixed Header*/
+    checkScroll(scrollOffset);
+
+    $(window).on("scroll", function () {
+        scrollOffset = $(this).scrollTop();
+
+        checkScroll(scrollOffset);
+
+    });
+
+    function checkScroll(scrollOffset) {
+
+        if (scrollOffset >= introH) {
+            header.addClass("fixed")
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+    /*Smooth scroll*/
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+        var blockId = $(this).data('scroll'),
+            blockOffset = $(blockId).offset().top;
+        
+        console.log(blockOffset);
+    });
+});
